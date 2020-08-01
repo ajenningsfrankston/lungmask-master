e0930098d0ef15e063d2e70774d1d0abb57957c4
Andrew Jennings <ajenningsfrankston@gmail.com>
2020-08-01 15:05:24 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lungmask",
+    version="0.2.8",
+    author="Johannes Hofmanninger",
+    author_email="johannes.hofmanninger@meduniwien.ac.at",
+    description="Package for automated lung segmentation in CT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JoHof/lungmask",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'lungmask = lungmask.__main__:main'
+        ]
+    },
+    install_requires=[
+        'pydicom',
+        'numpy',
+        'torch',
+        'scipy',
+        'SimpleITK',
+        'tqdm',
+        'scikit-image',
+        'fill_voids'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv3",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6',
+)
