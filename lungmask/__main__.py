71538919a9499527c380c4910dc22a4133cd26ea
Andrew Jennings <ajenningsfrankston@gmail.com>
2020-08-03 18:58:44 +1000

    modifying parameter setting

--- lungmask/__main__.py
+++ lungmask/__main__.py
@@ -17,30 +17,34 @@ def path(string):
 
 
 def main():
-    version = pkg_resources.require("lungmask")[0].version
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('input', metavar='input', type=path, help='Path to the input image, can be a folder for dicoms')
-    parser.add_argument('output', metavar='output', type=str, help='Filepath for output lungmask')
+    parser.add_argument('input', metavar='input', type=str, help='Path to the input image, can be a folder for dicoms')
+    parser.add_argument('output', metavar='output', type=str, help='Directory for output lungmask')
     parser.add_argument('--modeltype', help='Default: unet', type=str, choices=['unet'], default='unet')
     parser.add_argument('--modelname', help="spcifies the trained model, Default: R231", type=str, choices=['R231' ,'LTRCLobes','LTRCLobes_R231','R231CovidWeb'], default='R231')
     parser.add_argument('--cpu', help="Force using the CPU even when a GPU is available, will override batchsize to 1", action='store_true')
     parser.add_argument('--nopostprocess', help="Deactivates postprocessing (removal of unconnected components and hole filling", action='store_true')
     parser.add_argument('--noHU', help="For processing of images that are not encoded in hounsfield units (HU). E.g. png or jpg images from the web. Be aware, results may be substantially worse on these images", action='store_true')
     parser.add_argument('--batchsize', type=int, help="Number of slices processed simultaneously. Lower number requires less memory but may be slower.", default=20)
-    parser.add_argument('--version', help="Shows the current version of lungmask", action='version', version=version)
 
     argsin = sys.argv[1:]
+
+    print("argsin ",argsin)
+
     args = parser.parse_args(argsin)
-    
+
+    print("input file ",args.input)
+
     batchsize = args.batchsize
+
     if args.cpu:
         batchsize = 1
+    
+    input_image = utils.get_input_image(args.input)
 
     logging.info(f'Load model')
     
-    input_image = utils.get_input_image(args.input)
-    logging.info(f'Infer lungmask')
     if args.modelname == 'LTRCLobes_R231':
         result = mask.apply_fused(input_image, force_cpu=args.cpu, batch_size=batchsize, volume_postprocessing=not(args.nopostprocess), noHU=args.noHU)
     else:
